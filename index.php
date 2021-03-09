a1d9380f841e9e43e7c84ecd4456d22c00ae9b3d
matin <matin@debian.matin>
2021-03-09 18:50:00 +0330

    nem

---
+++ index.php
@@ -0,0 +1,119 @@
+﻿<?php 
+
+ob_start();
+/*
+به نام خدا
+@social_programming
+@amir_social
+_________________
+version:1
+*/
+
+$API_KEY = 'توکن';
+##------------------------------##
+define('API_KEY',$API_KEY);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+ function sendmessage($chat_id, $text, $model){
+	bot('sendMessage',[
+	'chat_id'=>$chat_id,
+	'text'=>$text,
+	'parse_mode'=>$mode
+	]);
+	}
+	function sendaction($chat_id, $action){
+	bot('sendchataction',[
+	'chat_id'=>$chat_id,
+	'action'=>$action
+	]);
+	}
+
+//====================special======================//
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$message_id = $update->message->id;
+$chat_id = $message->chat->id;
+$from_id = $message->from->id;
+$text = $message->text;
+$chatid = $update->callback_query->message->chat->id;
+$fromid = $update->callback_query->from->id;
+$username = $update->message->from->username;
+$bcpv = file_get_contents("bcpv.txt");
+$bcgap = file_get_contents("bcgap.txt");
+$name = $update->message->from->first_name;
+$gpname = $update->message->chat->title;
+$rt = $update->message->reply_to_message;
+$replyid = $update->message->reply_to_message->message_id;
+$rtid = $update->message->reply_to_message->from->id;
+$data = $update->callback_query->data;
+$photo = $update->message->photo;
+$forward = $update->message->forward_from;
+$video = $update->message->video;
+$location = $update->message->location;
+$sticker = $update->message->sticker;
+$document = $update->message->document;
+$contact = $update->message->contact;
+$game = $update->message->game;
+$music = $update->message->audio;
+$gif = $update->message->gif;
+$voice = $update->message->voice;
+$messageid = $update->callback_query->message->message_id;
+$forchaneel = json_decode(file_get_contents("https://api.telegram.org/bot$API_KEY/getChatMember?chat_id=@ایدی کانال&user_id=".$from_id)); 
+$tch = $forchaneel->result->status;
+$type = $update->message->chat->type;
+$get = file_get_contents("https://api.telegram.org/bot$API_KEY/getChatMember?chat_id=$chat_id&user_id=".$from_id);
+$info = json_decode($get, true);
+$rank = $info['result']['status'];
+$reply = $update->message->reply_to_message->message_id;
+//====================special=================//
+if($tch != 'member' && $tch != 'creator' && $tch != 'administrator'){
+SendMessage($chat_id,"سلام🌹
+$name 
+برا اینکه تو گروه بخوای چت کنی باید تو کانال ما جوین بشی وگرنه پیام هات پاک میشه
+بعد از ورود به کانال برگرد تو گروه و با خیال راحت از چت کردن لذت ببر
+ایدی کانال ما:
+🆔: @SOCIAL_PROGRAMMING");
+
+}
+
+if($rank != "creator" && $rank != "administrator"){ 
+if($text || $photo || $video || $location || $sticker || $document || $contact || $music || $gif || $voice){
+if($tch != 'member' && $tch != 'creator' && $tch != 'administrator'){
+bot('deleteMessage',[
+'chat_id'=>$chat_id,
+'message_id'=>$message->message_id
+  ]);
+}
+}
+}
+
+//############################################################################//
+if($text == "/social" && $from_id == 381292768){
+sendaction($chat_id, typing);
+        bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"
++  version:1 +   
+**************
+* coded by  : *
+*  @lyou4ul   *
+**************
+ @SOCIAL_PROGRAMMING ",
+        ]);
+  } 
+
+unlink ("error_log");
+	?>
+
+
